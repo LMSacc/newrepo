2e34cbba1805dfe5239cd1d0fd405574b77ba532
Ivan Kaziev <ivkaziev@yandex.ru>
2023-11-24 17:26:35 +0300

    asdasd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys
 import random
-from PyQt5 import uic
+from PyQt5 import uic, QtWidgets, QtCore
 from PyQt5.QtGui import QPainter, QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
@@ -11,6 +11,32 @@ class Form(QMainWindow):
         uic.loadUi('UI.ui', self)
         self.pushButton.clicked.connect(self.drawCircles)
         self.draw = False
+        self.initUI()
+
+    def initUI(self):
+        self.setObjectName("MainWindow")
+        self.resize(300, 300)
+        self.centralwidget = QtWidgets.QWidget(self)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(110, 110, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        self.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(self)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 300, 21))
+        self.menubar.setObjectName("menubar")
+        self.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(self)
+        self.statusbar.setObjectName("statusbar")
+        self.setStatusBar(self.statusbar)
+
+        self.retranslateUi(self)
+        QtCore.QMetaObject.connectSlotsByName(self)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "Нажми!"))
 
     def drawCircles(self):
         self.draw = True
